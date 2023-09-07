d8ae2232a380f32d29bc70110cf6d40e7715cbba
imerlog <nach.merlo@gmail.com>
2023-09-07 23:36:19 +0000

    primeros cambios

--- main.cpp
+++ main.cpp
@@ -3,18 +3,22 @@
 
 int main()
 {
+      //gasdetector es un objeto de la clase digital iny
+    // aca esta siendo creado mediante un constructar de forma gas(d2)
    DigitalIn gasDetector(D2);
    DigitalIn overTempDetector(D3);
    
    DigitalOut alarmLed(LED1);
    
    gasDetector.mode(PullDown);   
+   //.mode es un metodo del obejo gas detector
    overTempDetector.mode(PullDown);
 
    while (true) {
       
       if ( gasDetector || overTempDetector ) {
          alarmLed = ON;
+            printf("%s\n", "se detecto algo y el led  prendido");
       } else {
          alarmLed = OFF;
       }
